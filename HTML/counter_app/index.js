783904b95e00f528521f5ee3079d009748b97d61
08ops <danillo3412@gmail.com>
2026-04-18 15:25:22 +0000

    first commit
    first commit

---
+++ HTML/counter_app/index.js
@@ -0,0 +1,28 @@
+let count = document.getElementById('count')
+let saveEl = document.getElementById('save-el')
+
+console.log(count)
+
+
+let counter = 0
+ 
+ 
+ function increment() {
+    counter += 1
+    count.innerHTML = counter
+ }
+ 
+ function decrement() {
+    counter -= 1
+    count.innerHTML = counter
+    }
+    
+ function save() {
+
+   let countStr = counter + " - "
+   saveEl.innerHTML += countStr
+   console.log(counter)
+ }
+
+
+
---
+++ HTML/welcome/index.js
@@ -0,0 +1,6 @@
+let message = document.getElementById('welcome-el')
+
+let name = "John"
+let greeting = "Welcome back, "
+
+message.innerHTML = greeting + name + "!"
